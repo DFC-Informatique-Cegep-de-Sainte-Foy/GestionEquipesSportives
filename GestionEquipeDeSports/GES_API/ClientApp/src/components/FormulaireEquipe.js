809fcf8ab03ed76bd7d0097b301b9aabde38b975
Oleksii <36396435+Olmehan@users.noreply.github.com>
2023-03-03 19:26:08 -0500

    ajout d'une page web formulaire equipe

---
+++ GestionEquipeDeSports/GES_API/ClientApp/src/components/FormulaireEquipe.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button'
+
+export class FormEquipe extends React.Component{
+    render(){
+        return(
+            <>
+                <h2>Ajouter une equipe</h2>
+                <Form>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Sport</Form.Label>
+                        <Form.Select id="id_sport"  >
+                            <option disabled>Choisir une option</option>
+                            <option value="soccer">Soccer</option>
+                            <option value="baseball">Baseball</option>
+                            <option value="football">Football</option>
+                            <option value="natation">Natation</option>
+                        </Form.Select>
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Nom Equipe</Form.Label>
+                        <Form.Control type="text"  />
+                    </Form.Group>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Description</Form.Label>
+                        <Form.Control type="text"  />
+                    </Form.Group>
+                    <Button variant="succes">Ajouter</Button>
+                    <Button variant="danger">Annuler</Button>
+                </Form>
+            </>
+        );
+    }
+}
