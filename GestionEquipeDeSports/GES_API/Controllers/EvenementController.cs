57da722bb0c7d98f98dcb40d57e4f6e742acfd00
1262695 <1262695@csfoy.ca>
2023-03-06 14:17:21 -0500

    API evenement et classe Model fait

--- GestionEquipeDeSports/GES_API/Controllers/EvenementController.cs
+++ GestionEquipeDeSports/GES_API/Controllers/EvenementController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using GES_Services.Manipulations;
+using GES_API.Models;
+using System.Diagnostics.CodeAnalysis;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,36 +11,82 @@ namespace GES_API.Controllers
     [ApiController]
     public class EvenementController : ControllerBase
     {
+        
+        private ManiulationDepotEvenement m_maniulationDepotEvenement;
+        public EvenementController(ManiulationDepotEvenement p_maniulationDepotEvenement)
+        {
+            this.m_maniulationDepotEvenement = p_maniulationDepotEvenement;
+        }
         // GET: api/<EvenementController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        [ProducesResponseType(200)]
+        public ActionResult<IEnumerable<EvenementModel>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return Ok(this.m_maniulationDepotEvenement.ListerEvenements);
         }
 
         // GET api/<EvenementController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        public ActionResult<EvenementModel> Get(int id)
         {
-            return "value";
+            EvenementModel model = new EvenementModel(this.m_maniulationDepotEvenement.ChercherEvenementParId(id));
+            if (model != null)
+            {
+                return Ok(model);
+            }
+            return NotFound();
         }
 
         // POST api/<EvenementController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        public ActionResult Post([FromBody] EvenementModel p_evenementModel)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            m_maniulationDepotEvenement.AjouterEvenement(p_evenementModel.VersEntite());
+            return Ok();
         }
 
         // PUT api/<EvenementController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public ActionResult Put(int p_id, [FromBody] EvenementModel p_evenementModel)
         {
+            if (!ModelState.IsValid || p_evenementModel.Id != p_id)
+            {
+                return BadRequest();
+            }
+            EvenementModel model = new EvenementModel(m_maniulationDepotEvenement.ChercherEvenementParId(p_id));
+            if (model is null)
+            {
+                return NotFound();
+            }
+            m_maniulationDepotEvenement.ModifierEvenement(p_evenementModel.VersEntite());
+            return NoContent();
         }
 
         // DELETE api/<EvenementController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public ActionResult Delete(int id)
         {
+            EvenementModel model = new EvenementModel(m_maniulationDepotEvenement.ChercherEvenementParId(p_id));
+            if (model is null)
+            {
+                return NotFound();
+            }
+
+            m_maniulationDepotEvenement.SupprimerEvenement(model.VersEntite());
+            return NoContent();
         }
     }
 }
