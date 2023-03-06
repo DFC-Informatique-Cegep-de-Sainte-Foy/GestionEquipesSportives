c926c7d951a6303ac88ab04a19793488e01d5d20
Oleksii <36396435+Olmehan@users.noreply.github.com>
2023-03-06 14:43:01 -0500

    ajout iDepotEquipe.cs

---
+++ GestionEquipeDeSports/GES_Services/Interfaces/IDepotEquipe.cs
@@ -0,0 +1,18 @@
+﻿using GES_Services.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GES_Services.Interfaces
+{
+    public interface IDepotEquipe
+    {
+        public bool AjouterEquipe(Equipe equipe);
+        public bool ModifierEquipe(Equipe equipe);
+        public bool SupprimerEquipe(Equipe equipe);
+        public Equipe ChercherEquipeParId(int id);
+        public IEnumerable<Equipe> ListerEquipe();
+    }
+}
