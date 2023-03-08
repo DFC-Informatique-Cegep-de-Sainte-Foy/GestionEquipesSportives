2eaca73a93525eb690564ea6b2becde776576389
Oleksii <36396435+Olmehan@users.noreply.github.com>
2023-03-08 01:09:31 -0500

    creation equipe controller et methode get

--- GestionEquipeDeSports/GES_API/Controllers/EquipeController.cs
+++ GestionEquipeDeSports/GES_API/Controllers/EquipeController.cs
@@ -1,6 +1,39 @@
-﻿namespace GES_API.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using GES_Services.Manipulations;
+using GES_API.Models;
+
+namespace GES_API.Controllers
 {
-    public class EquipeController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EquipeController : ControllerBase
     {
+        private ManipulationDepotEquipe m_manipulationDepotEquipe;
+        public EquipeController(ManipulationDepotEquipe p_manipulationDepotEquipe)
+        {
+            this.m_manipulationDepotEquipe = p_manipulationDepotEquipe;
+        }
+
+        //GET: api/<EquipeController>
+        [HttpGet]
+        [ProducesResponseType(200)]
+        public ActionResult<IEnumerable<EquipeModel>> Get()
+        {
+            return Ok(this.m_manipulationDepotEquipe.ListerEquipe);
+        }
+
+        //Get: api/<EquipeController>/5
+        [HttpGet("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public ActionResult<EquipeModel> Get(int id)
+        {
+            EquipeModel model = new EquipeModel(this.m_manipulationDepotEquipe.ChercherEquipeParId(id));
+            if(model != null)
+            {
+                return Ok(model);
+            }
+            else { return NotFound(); }
+        }
     }
 }
