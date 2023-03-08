042ce803c5634234fc66c3e7a1ce621501315145
Habil-Ndiamoun <85562269+Habil-Ndiamoun@users.noreply.github.com>
2023-03-08 11:18:39 -0500

    Ajout des routes dans EquipeController

--- GestionEquipeDeSports/GES_API/Controllers/EquipeController.cs
+++ GestionEquipeDeSports/GES_API/Controllers/EquipeController.cs
@@ -19,7 +19,7 @@ namespace GES_API.Controllers
         [ProducesResponseType(200)]
         public ActionResult<IEnumerable<EquipeModel>> Get()
         {
-            return Ok(this.m_manipulationDepotEquipe.ListerEquipe);
+            return Ok(this.m_manipulationDepotEquipe.ListerEquipes());
         }
 
         //Get: api/<EquipeController>/5
@@ -33,7 +33,77 @@ namespace GES_API.Controllers
             {
                 return Ok(model);
             }
-            else { return NotFound(); }
+            else 
+            { 
+                return NotFound(); 
+            }
         }
+
+        // POST api/<EquipeController>
+        [HttpPost]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)]
+        public ActionResult Post([FromBody] EquipeModel p_equipeModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            this.m_manipulationDepotEquipe.AjouterEquipe(p_equipeModel.VersEntite());
+            return Ok();
+        }
+
+        // PUT api/<EquipeController>/5
+        [HttpPut("{id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public ActionResult Put(int p_id, [FromBody] EquipeModel p_equipeModel)
+        {
+            if (!ModelState.IsValid || p_equipeModel.Id != p_id)
+            {
+                return BadRequest();
+            }
+
+            EquipeModel equipeModel = new EquipeModel(this.m_manipulationDepotEquipe.ChercherEquipeParId(p_id));
+
+            if (equipeModel is null)
+            {
+                return NotFound();
+            }
+
+            this.m_manipulationDepotEquipe.ModifierEquipe(p_equipeModel.VersEntite());
+
+            return NoContent();
+        }
+
+        // DELETE api/<EquipeController>/5
+        [HttpDelete("{id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public ActionResult Delete(int p_id)
+        {
+            EquipeModel equipeModel = new EquipeModel(this.m_manipulationDepotEquipe.ChercherEquipeParId(p_id));
+
+            if (equipeModel is null)
+            {
+                return NotFound();
+            }
+
+            this.m_manipulationDepotEquipe.SupprimerEquipe(equipeModel.VersEntite());
+
+            return NoContent();
+        }
+
+
+
+
+
+
+
+
+
+
     }
 }
