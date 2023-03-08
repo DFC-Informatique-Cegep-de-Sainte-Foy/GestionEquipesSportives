5f47ef6d291921bb52ed59fe83158062d7c9fc27
Oleksii <36396435+Olmehan@users.noreply.github.com>
2023-03-08 00:18:33 -0500

    ajout equipeModel.cs

---
+++ GestionEquipeDeSports/GES_API/Models/EquipeModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using GES_Services.Entites;
+
+namespace GES_API.Models
+{
+    public class EquipeModel
+    {
+        [Key] public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Region { get; set; }
+        public DateTime DateCreation { get; set; }
+        public DateTime DateModification { get; set; }
+        public bool Etat { get; set; }
+        public string Sport { get; set; }
+        public string AssociationSportive { get; set; }
+
+        public EquipeModel()
+        {
+            ;
+        }
+
+        public EquipeModel(GES_Services.Entites.Equipe p_equipe)
+        {
+            this.Id = p_equipe.Id;
+            this.Nom = p_equipe.Nom;
+            //this.Region = p_equipe.
+            this.DateCreation = p_equipe.DateCreation;
+            this.DateModification = p_equipe.DateModification;
+        }
+
+        public GES_Services.Entites.Equipe VersEntite()
+        {
+            return new GES_Services.Entites.Equipe();
+        }
+    }
+}
