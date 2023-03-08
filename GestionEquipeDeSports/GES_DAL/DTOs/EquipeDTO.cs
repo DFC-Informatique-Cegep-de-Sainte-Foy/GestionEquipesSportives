2fb9a0f6dc3339df334ae8c1cdecb95ac2fd4dde
Kevin C <92613076+kev-42@users.noreply.github.com>
2023-03-08 12:24:38 -0500

    ajout KEY dans les DTO

--- GestionEquipeDeSports/GES_DAL/DTOs/EquipeDTO.cs
+++ GestionEquipeDeSports/GES_DAL/DTOs/EquipeDTO.cs
@@ -9,7 +9,7 @@ namespace GES_DAL.DTOs
 {
     public class EquipeDTO
     {
-        public int Id { get; set; }
+        [Key] public int Id { get; set; }
         public string Nom { get; set; }
         public string Description { get; set; }
         public DateTime DateCreation { get; set; }
--- GestionEquipeDeSports/GES_DAL/DTOs/EvenementDTO.cs
+++ GestionEquipeDeSports/GES_DAL/DTOs/EvenementDTO.cs
@@ -9,7 +9,7 @@ namespace GES_DAL.DTOs
 {
     public class EvenementDTO
     {
-        public int Id { get; set; }
+        [Key] public int Id { get; set; }
         public string Description { get; set; }
         public DateTime DateDebut { get; set; }
         public DateTime DateFin { get; set; }
