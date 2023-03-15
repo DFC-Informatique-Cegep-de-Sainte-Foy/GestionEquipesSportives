a4743619da27c69ac9dfe7d40a655ab88df2678f
1262695 <1262695@csfoy.ca>
2023-03-15 14:32:54 -0400

    Creation Controleur pour evenementcsv

---
+++ GestionEquipeDeSports/GES_Services/Interfaces/IDepotImportationEvenementCSV.cs
@@ -0,0 +1,16 @@
+﻿using GES_Services.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GES_Services.Interfaces
+{
+    public interface IDepotImportationEvenementCSV
+    {
+        public void AjouterEvenements(List<Evenement> p_evenements);
+        public IEnumerable<Evenement> LireEvenements(string p_nomFichierAImporter);
+
+    }
+}
