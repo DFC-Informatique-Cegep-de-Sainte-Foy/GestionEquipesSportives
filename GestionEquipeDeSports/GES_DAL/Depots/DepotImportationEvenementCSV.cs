4bc19d74f35e80598e95f95d30a88d0dadc52263
1262695 <1262695@csfoy.ca>
2023-03-15 11:04:49 -0400

    creation interface et classe depot

---
+++ GestionEquipeDeSports/GES_DAL/Depots/DepotImportationEvenementCSV.cs
@@ -0,0 +1,49 @@
+﻿using GES_Services.Entites;
+using GES_Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GES_DAL.Depots
+{
+    internal class DepotImportationEvenementCSV : IDepotImportationEvenement
+    {
+        private readonly string separateurChamps = "\",\"";
+
+        private string m_nomFichierAImporter;
+
+        public DepotImportationEvenementCSV(string p_nomFichierAImporter)
+        {
+            if (string.IsNullOrWhiteSpace(p_nomFichierAImporter))
+            {
+                throw new ArgumentOutOfRangeException(nameof(p_nomFichierAImporter));
+            }
+
+            if (!File.Exists(p_nomFichierAImporter))
+            {
+                throw new InvalidOperationException($"Impossible de trouver le fichier {p_nomFichierAImporter}");
+            }
+
+            this.m_nomFichierAImporter = p_nomFichierAImporter;
+        }
+        public IEnumerable<Evenement> LireEvenements()
+        {
+            List<Evenement> evenements = new List<Evenement>(); 
+
+            using (StreamReader sr = File.OpenText(m_nomFichierAImporter))
+            {
+                string ligneCourante;
+                int numLigneCourante = 0;
+                while (!sr.EndOfStream)
+                {
+                    ligneCourante = sr.ReadLine();
+                    ++numLigneCourante;
+                }
+                sr.Close();
+            }
+            return evenements;
+        }
+    }
+}
---
+++ GestionEquipeDeSports/GES_Services/Interfaces/IDepotImportationEvenement.cs
@@ -0,0 +1,14 @@
+﻿using GES_Services.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GES_Services.Interfaces
+{
+    public interface IDepotImportationEvenement
+    {
+        IEnumerable<Evenement> LireEvenements();
+    }
+}
