aa548b469bcddec05e71d6b1fff54e7729f6a492
Habil-Ndiamoun <85562269+Habil-Ndiamoun@users.noreply.github.com>
2023-04-05 16:47:45 -0400

    update

--- GestionEquipeDeSports/GES_API/ClientApp/src/components/Erreur404.js
+++ GestionEquipeDeSports/GES_API/ClientApp/src/components/Erreur404.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Alert from 'react-bootstrap/Alert'
+import { Link } from 'react-router-dom';
 
 function Erreur404(){
     return(
-        <Alert variant="danger">
-            <h1>Erreur 404 : La ressource sollicitée n'existe pas</h1>
-        </Alert>
+        <>
+            <Alert variant="danger">
+                <h1>Erreur 404</h1>
+            </Alert>
+            <br></br>
+            <h1>Nous n'avons pas trouvé la page que vous cherchiez.</h1>
+            <br></br>
+            <p>Vous avez peut-être mal tapé l'adresse ou la page a été déplacée ou supprimée.</p>
+            <br></br>
+            <p>Vous pouvez rechercher plus de résultats ou vous rendre sur notre <Link to="/">page d'accueil.</Link></p>
+        </>
+        
     );
 }
 
