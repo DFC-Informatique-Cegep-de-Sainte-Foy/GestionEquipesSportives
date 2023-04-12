57a94c953ca5aaf4281939cd289c8b707bc43fda
Habil-Ndiamoun <85562269+Habil-Ndiamoun@users.noreply.github.com>
2023-04-12 10:21:45 -0400

    update

--- GestionEquipeDeSports/GES_API/ClientApp/src/App.js
+++ GestionEquipeDeSports/GES_API/ClientApp/src/App.js
@@ -7,31 +7,26 @@ import './custom.css';
 import { PageLoader } from "./components/page-loader";
 import { useAuth0 } from '@auth0/auth0-react';
 
-
-//export default class App extends Component {
-  //static displayName = App.name;
-  export default function App(){
-  //render() {
-    const { isLoading } = useAuth0();
+export default function App(){
+  const { isLoading } = useAuth0();
     
-    if (isLoading){
-      return(
-        <div className="page-layout">
-          <PageLoader />
-        </div>
-      );
-    }
-
-    return (
-      <Layout>
-        <Routes>
-          {AppRoutes.map((route, index) => {
-            const { element, ...rest } = route;
-            return <Route key={index} {...rest} element={element} />;
-          })}
-        </Routes>
-      </Layout>
+  if (isLoading){
+    return(
+      <div className="page-layout">
+        <PageLoader />
+      </div>
     );
-  //}
+  }
+
+  return (
+    <Layout>
+      <Routes>
+        {AppRoutes.map((route, index) => {
+          const { element, ...rest } = route;
+          return <Route key={index} {...rest} element={element} />;
+        })}
+      </Routes>
+    </Layout>
+  );
 }
 
