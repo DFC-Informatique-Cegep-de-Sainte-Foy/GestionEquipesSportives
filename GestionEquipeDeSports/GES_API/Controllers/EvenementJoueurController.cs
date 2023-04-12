dfcb6ec40a31e0738d7dbc1a68e1a7db3b8d8d2c
kev-42 <92613076+kev-42@users.noreply.github.com>
2023-04-12 14:05:04 -0400

    update controller et model evenementJoueur

--- GestionEquipeDeSports/GES_API/Controllers/EvenementJoueurController.cs
+++ GestionEquipeDeSports/GES_API/Controllers/EvenementJoueurController.cs
@@ -9,13 +9,64 @@ namespace GES_API.Controllers
     public class EvenementJoueurController : ControllerBase
     {
         private ManipulationDepotEvenementJoueur m_manipulationDepotEvenementJoueur;
+        
         public EvenementJoueurController(ManipulationDepotEvenementJoueur p_manipulationDepotEvenementJoueur)
         {
             if(p_manipulationDepotEvenementJoueur == null)
             {
                 throw new ArgumentNullException(nameof(p_manipulationDepotEvenementJoueur));
             }
+            
             this.m_manipulationDepotEvenementJoueur = p_manipulationDepotEvenementJoueur;
+        }
+
+        //GET: api/<EvenementJoueurController>/5
+        [HttpGet("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public ActionResult<IEnumerable<UtilisateurModel>> Get(Guid id)
+        {
+            //IEnumerable<UtilisateurModel> listeJoueurs;
+            //try
+            //{
+            //    listeJoueurs = this.m_manipulationDepotEquipeJoueur.ListerEquipeJoueurs(id).Select(e => new UtilisateurModel(e));
+            //}
+            //catch (Exception e)
+            //{
+            //    return BadRequest(e.Message);
+            //}
+            //if (listeJoueurs != null)
+            //{
+            //    return Ok(listeJoueurs);
+            //}
+            //else
+            //{
+            //    return NotFound();
+            //}
+
+            return NotFound();
+        }
+
+
+        //Put api/<EvenementJoueurController>/5
+        [HttpPut("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public ActionResult Put(Guid id, [FromBody] EvenementJoueurModel p_evenementJoueurModel)
+        {
+            if (p_evenementJoueurModel == null)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                this.m_manipulationDepotEvenementJoueur.AjouterPresencePourJoueur(p_evenementJoueurModel.DeModelVersEntite());
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+            return Ok();
         }
     }
 }
