a7b1922b23af4047d04d333bfb55d2b7c2f07190
Kevin C <92613076+kev-42@users.noreply.github.com>
2023-04-13 13:52:46 -0400

    update

--- Database/scriptBD/Evenements.sql
+++ Database/scriptBD/Evenements.sql
@@ -15,15 +15,15 @@
 --);
 --GO
 
-INSERT INTO [Evenement] (Description,Emplacement,dateDebut,dateFin,Fk_Id_TypeEvenement)
+INSERT INTO [Evenement] (Description,Emplacement,dateDebut,dateFin, Etat, Fk_Id_TypeEvenement)
 VALUES
-  ('sem, consequat nec,','Nunc commodo','Apr 20, 2022 11:44','Apr 21, 2022 11:44',1),
-  ('Aliquam fringilla cursus','accumsan interdum','Apr 20, 2022 11:44','Apr 21, 2022 11:44',1),
-  ('Cras convallis convallis','sodales purus,','Apr 20, 2022 11:44','Apr 21, 2022 11:44',1),
-  ('montes, nascetur ridiculus','purus ac','Apr 20, 2022 11:44','Apr 21, 2022 11:44',1),
-  ('egestas. Duis ac','mauris, aliquam','Apr 20, 2022 11:44','Apr 21, 2022 11:44',1),
-  ('morbi tristique senectus','sit amet,','Apr 20, 2022 11:44','Apr 21, 2022 11:44',2),
-  ('vulputate, nisi sem','Nulla facilisis.','Apr 20, 2022 11:44','Apr 21, 2022 11:44',0),
-  ('libero. Morbi accumsan','at sem','Apr 20, 2022 11:44','Apr 21, 2022 11:44',1),
-  ('arcu vel quam','amet, dapibus','Apr 20, 2022 11:44','Apr 21, 2022 11:44',1),
-  ('lorem, eget mollis','Cras pellentesque.','Apr 20, 2022 11:44','Apr 21, 2022 11:44',1);
+  ('sem, consequat nec,','Nunc commodo','Apr 20, 2022 11:44','Apr 21, 2022 11:44',1,1),
+  ('Aliquam fringilla cursus','accumsan interdum','Apr 20, 2022 11:44','Apr 21, 2022 11:44',1,1),
+  ('Cras convallis convallis','sodales purus,','Apr 20, 2022 11:44','Apr 21, 2022 11:44',1,1),
+  ('montes, nascetur ridiculus','purus ac','Apr 20, 2022 11:44','Apr 21, 2022 11:44',1,1),
+  ('egestas. Duis ac','mauris, aliquam','Apr 20, 2022 11:44','Apr 21, 2022 11:44',1,1),
+  ('morbi tristique senectus','sit amet,','Apr 20, 2022 11:44','Apr 21, 2022 11:44',1,2),
+  ('vulputate, nisi sem','Nulla facilisis.','Apr 20, 2022 11:44','Apr 21, 2022 11:44',1,0),
+  ('libero. Morbi accumsan','at sem','Apr 20, 2022 11:44','Apr 21, 2022 11:44',1,1),
+  ('arcu vel quam','amet, dapibus','Apr 20, 2022 11:44','Apr 21, 2022 11:44',1,1),
+  ('lorem, eget mollis','Cras pellentesque.','Apr 20, 2022 11:44','Apr 21, 2022 11:44',1,1);
