7f33d1be5d651b61a4037b5ec7ce820ca5a47521
Kevin C <92613076+kev-42@users.noreply.github.com>
2023-04-25 21:36:01 -0400

    indentation

--- GestionEquipeDeSports/GES_API/ClientApp/src/pages/PageAccueilEntraineur.js
+++ GestionEquipeDeSports/GES_API/ClientApp/src/pages/PageAccueilEntraineur.js
@@ -4,13 +4,13 @@ import { useNavigate } from "react-router-dom";
 import { useAuth0 } from '@auth0/auth0-react';
 import { BiCheck, BiX } from "react-icons/bi";
 
-const CalculerDuree = (props) =>{
+const CalculerDuree = (props) => {
     let dateDebut = props.dateACalculer[0];
     let dateFin = props.dateACalculer[1];
     let dateDebutParsed = Date.parse(dateDebut);
     let dateFinParsed = Date.parse(dateFin);
     let result = (dateFinParsed - dateDebutParsed) / 60000;
-    if(result > 60){
+    if (result > 60) {
         let dureeH = result / 60;
         let dureeM = result % 60;
         return (
@@ -19,7 +19,7 @@ const CalculerDuree = (props) =>{
             </td>
         )
     }
-    else{
+    else {
         return (
             <td>
                 {result}min
@@ -28,7 +28,7 @@ const CalculerDuree = (props) =>{
     }
 }
 
-const FormatDateTime = (props) =>{    
+const FormatDateTime = (props) => {
     var dateTimeEntree = props.doneesDateTime;
     var date = dateTimeEntree.split('T').join(' ');
     var dateFormatee = date.substring(0, 16);
@@ -36,7 +36,7 @@ const FormatDateTime = (props) =>{
         <td>
             {dateFormatee}
         </td>
-    )    
+    )
 }
 
 export const PageAcceuilEntraineur = () => {
@@ -69,28 +69,28 @@ export const PageAcceuilEntraineur = () => {
     }, [evenements]);
 
     //trouver utilisateur dans BD par son email
-    async function getUtilisateur(email){
+    async function getUtilisateur(email) {
         const token = await getAccessTokenSilently();
 
         await fetch(`api/utilisateur/${email}`, {
             headers: { Accept: "application/json", Authorization: `Bearer ${token}` },
         })
-        .then(res => res.json())
-        .then((result) => {
-            console.log(result);
-            setUtilisateur(result);
-            setRoleUtilisateur(result.roles);
-            setIdUtilisateur(result.idUtilisateur);
-        });
+            .then(res => res.json())
+            .then((result) => {
+                console.log(result);
+                setUtilisateur(result);
+                setRoleUtilisateur(result.roles);
+                setIdUtilisateur(result.idUtilisateur);
+            });
     }
 
-    async function getEquipesDeJoueur(id){
+    async function getEquipesDeJoueur(id) {
         const token = await getAccessTokenSilently();
-console.log('Id Utilisateur');
-console.log(id);
+        console.log('Id Utilisateur');
+        console.log(id);
         await fetch(`api/UtilisateurEquipe/${id}`, {
             headers: { Accept: "application/json", Authorization: `Bearer ${token}` },
-          })
+        })
             .then(res => res.json())
             .then((result) => {
                 console.log('Equipes :');
@@ -101,12 +101,12 @@ console.log(id);
             });
     }
 
-    async function getEvenementsDeJoueur(id){
+    async function getEvenementsDeJoueur(id) {
         const token = await getAccessTokenSilently();
 
         await fetch(`api/evenementJoueur/${id}`, {
             headers: { Accept: "application/json", Authorization: `Bearer ${token}` },
-          })
+        })
             .then(res => res.json())
             .then((result) => {
                 console.log('Evenements :');
@@ -117,21 +117,21 @@ console.log(id);
             });
     }
 
-    function trouverEvenementsPourUtilisateur(){
+    function trouverEvenementsPourUtilisateur() {
         const ev = Array.isArray(evenements) ? evenements.flatMap((ev) => obtenirEvenementAPartirSonId(ev.fk_Id_Evenement)) : [];
     }
 
-    function convertirPresence(donnees){
+    function convertirPresence(donnees) {
         let etatAReturn;
-        if(donnees === true){
-            etatAReturn = <td style={{color: "green"}}>PRESENT</td>
-        }else{
-            etatAReturn = <td style={{color: "red"}}>absent</td>
+        if (donnees === true) {
+            etatAReturn = <td style={{ color: "green" }}>PRESENT</td>
+        } else {
+            etatAReturn = <td style={{ color: "red" }}>absent</td>
         }
         return etatAReturn;
     }
 
-    async function obtenirEvenementAPartirSonId(idEvenement){
+    async function obtenirEvenementAPartirSonId(idEvenement) {
         // let arrayEvenements = [];
         // arrayEvenements = utilisateurEvenement;
         const token = await getAccessTokenSilently();
@@ -140,87 +140,87 @@ console.log(id);
         })
             .then(res => res.json())
             .then((result) => {
-                console.log(result); 
+                console.log(result);
                 // setUtilisateurEvenement(result);
                 // arrayEvenements.push(result);
                 setUtilisateurEvenement([...utilisateurEvenement, result]);
-        }).catch(function (error) {
-            console.log(error);
-        });
+            }).catch(function (error) {
+                console.log(error);
+            });
     }
-console.log(utilisateurEvenement);
+    console.log(utilisateurEvenement);
     return (
         <>
-        <Container>
-            <Row>
-                <Col>
-                    <h5>Liste des équipes</h5>
-                </Col>
-                <Col>Bonjour {utilisateur.prenom}!</Col>
-                <Col>
-                    <Button variant="success" className="float-end" onClick={() => navigate("/formulaireEquipe")}>Créer une équipe</Button>
-                </Col>
-            </Row>
-            <p></p>
-            <Row style={{maxHeight: "200px", overflow: "auto"}}>
-                <Table striped bordered >
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Nom</th>
-                            <th>Region</th>
-                            <th>Sport</th>
-                            <th>Association sportive</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {equipes.map((e, index) => (
-                            <tr key={e.idEquipe} onClick={() => navigate(`/uneEquipe/${e.idEquipe}`)} style={{cursor: "pointer"}}>
-                                <td>{index + 1}</td>
-                                <td>{e.nom}</td>
-                                <td>{e.region}</td>
-                                <td>{e.sport}</td>
-                                <td>{e.associationSportive}</td>
+            <Container>
+                <Row>
+                    <Col>
+                        <h5>Liste des équipes</h5>
+                    </Col>
+                    <Col>Bonjour {utilisateur.prenom}!</Col>
+                    <Col>
+                        <Button variant="success" className="float-end" onClick={() => navigate("/formulaireEquipe")}>Créer une équipe</Button>
+                    </Col>
+                </Row>
+                <p></p>
+                <Row style={{ maxHeight: "200px", overflow: "auto" }}>
+                    <Table striped bordered >
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Nom</th>
+                                <th>Region</th>
+                                <th>Sport</th>
+                                <th>Association sportive</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </Table>
-            </Row>
-            <Row style={{marginTop: "1.5em"}}>
-                <Col>
-                    <h5>Vos événements à venir</h5>
-                </Col>
-            </Row>
-            <Row>
-                <Table striped bordered>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Nom événement</th>
-                            <th>Date début</th>
-                            <th>Durée</th>
-                            <th>Presence</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {Array.isArray(utilisateurEvenement) ? utilisateurEvenement.map((e, index) => (
-                            <tr key={e.id}>
-                                <td>{index + 1}</td>
-                                <td>{e.description}</td>
-                                <FormatDateTime doneesDateTime={e.dateDebut} />
-                                <CalculerDuree dateACalculer={[e.dateDebut, e.dateFin]} />
-                                <td>Présence sera ici</td>
-                                {/*convertirPresence(e.estPresentAevenement)*/}
-                                <td><Button variant='success' size="sm" className="me-2" title="Est présent"> <BiCheck /></Button>
-                                    <Button variant='warning' size="sm" className="me-2" title="Est absent"> <BiX /></Button>
-                                </td>
+                        </thead>
+                        <tbody>
+                            {equipes.map((e, index) => (
+                                <tr key={e.idEquipe} onClick={() => navigate(`/uneEquipe/${e.idEquipe}`)} style={{ cursor: "pointer" }}>
+                                    <td>{index + 1}</td>
+                                    <td>{e.nom}</td>
+                                    <td>{e.region}</td>
+                                    <td>{e.sport}</td>
+                                    <td>{e.associationSportive}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                </Row>
+                <Row style={{ marginTop: "1.5em" }}>
+                    <Col>
+                        <h5>Vos événements à venir</h5>
+                    </Col>
+                </Row>
+                <Row>
+                    <Table striped bordered>
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Nom événement</th>
+                                <th>Date début</th>
+                                <th>Durée</th>
+                                <th>Presence</th>
+                                <th></th>
                             </tr>
-                        )) : []}
-                    </tbody>
-                </Table>
-            </Row>
-        </Container>
+                        </thead>
+                        <tbody>
+                            {Array.isArray(utilisateurEvenement) ? utilisateurEvenement.map((e, index) => (
+                                <tr key={e.id}>
+                                    <td>{index + 1}</td>
+                                    <td>{e.description}</td>
+                                    <FormatDateTime doneesDateTime={e.dateDebut} />
+                                    <CalculerDuree dateACalculer={[e.dateDebut, e.dateFin]} />
+                                    <td>Présence sera ici</td>
+                                    {/*convertirPresence(e.estPresentAevenement)*/}
+                                    <td><Button variant='success' size="sm" className="me-2" title="Est présent"> <BiCheck /></Button>
+                                        <Button variant='warning' size="sm" className="me-2" title="Est absent"> <BiX /></Button>
+                                    </td>
+                                </tr>
+                            )) : []}
+                        </tbody>
+                    </Table>
+                </Row>
+            </Container>
         </>
     )
 }
