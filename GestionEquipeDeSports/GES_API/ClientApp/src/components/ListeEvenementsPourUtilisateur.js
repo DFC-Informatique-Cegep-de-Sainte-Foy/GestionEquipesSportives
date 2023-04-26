d577147915720e2fdf23a1f36a3e2bcc7e9c8ed3
Kevin C <92613076+kev-42@users.noreply.github.com>
2023-04-26 15:37:17 -0400

    indentation

--- GestionEquipeDeSports/GES_API/ClientApp/src/components/ListeEvenementsPourUtilisateur.js
+++ GestionEquipeDeSports/GES_API/ClientApp/src/components/ListeEvenementsPourUtilisateur.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Evenement } from "./Evenement";
 
-export const ListeEvenementsPourUtilisateur = (props) =>{
+export const ListeEvenementsPourUtilisateur = (props) => {
     console.log(props.ev);
     var evenementsPourUtilisateur = props.ev;
     console.log('Function ListeEvenementsPourUtilisateur :');
@@ -9,25 +9,25 @@ export const ListeEvenementsPourUtilisateur = (props) =>{
     return (
         <tbody>
             {Array.isArray(evenementsPourUtilisateur) ? evenementsPourUtilisateur.map((e, index) => {
-                return <Evenement 
-                        key={e.id}
-                        id={e.id}
-                        num={index+1}
-                        description={e.description}
-                        emplacement={e.emplacement}
-                        dateDebut={e.dateDebut}
-                        dateFin={e.dateFin}
-                    />
-            }) : <Evenement 
-                    key={evenementsPourUtilisateur.id}
-                    id={evenementsPourUtilisateur.id}
-                    num={1}
-                    description={evenementsPourUtilisateur.description}
-                    emplacement={evenementsPourUtilisateur.emplacement}
-                    dateDebut={evenementsPourUtilisateur.dateDebut}
-                    dateFin={evenementsPourUtilisateur.dateFin}
-                />}
+                return <Evenement
+                    key={e.id}
+                    id={e.id}
+                    num={index + 1}
+                    description={e.description}
+                    emplacement={e.emplacement}
+                    dateDebut={e.dateDebut}
+                    dateFin={e.dateFin}
+                />
+            }) : <Evenement
+                key={evenementsPourUtilisateur.id}
+                id={evenementsPourUtilisateur.id}
+                num={1}
+                description={evenementsPourUtilisateur.description}
+                emplacement={evenementsPourUtilisateur.emplacement}
+                dateDebut={evenementsPourUtilisateur.dateDebut}
+                dateFin={evenementsPourUtilisateur.dateFin}
+            />}
         </tbody>
-        
+
     )
 }
