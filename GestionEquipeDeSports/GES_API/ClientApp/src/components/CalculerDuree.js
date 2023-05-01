5873a718ecf41f1dc13949256769cf4f8ec28451
Kevin C <92613076+kev-42@users.noreply.github.com>
2023-05-01 09:50:03 -0400

    indentation

--- GestionEquipeDeSports/GES_API/ClientApp/src/components/CalculerDuree.js
+++ GestionEquipeDeSports/GES_API/ClientApp/src/components/CalculerDuree.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-export const CalculerDuree = (props) =>{
+export const CalculerDuree = (props) => {
     var dateDebut = props.dateACalculer[0];
     var dateFin = props.dateACalculer[1];
     var dateDebutParsed = Date.parse(dateDebut);
     var dateFinParsed = Date.parse(dateFin);
     var result = (dateFinParsed - dateDebutParsed) / 60000;
-    if(result > 60){
+    if (result > 60) {
         var dureeH = result / 60;
         dureeH = dureeH.toFixed();
         var dureeM = result % 60;
@@ -16,7 +16,7 @@ export const CalculerDuree = (props) =>{
             </td>
         )
     }
-    else{
+    else {
         return (
             <td>
                 {result}min
