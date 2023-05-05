bd6c0a19dd03a65aff286daa8533eb6fa98a644c
Kevin C <92613076+kev-42@users.noreply.github.com>
2023-05-05 17:22:28 -0400

    update

--- Database/scriptBD/local/EquipeJoueur-local.sql
+++ Database/scriptBD/local/EquipeJoueur-local.sql
@@ -3,8 +3,11 @@
 
 insert into [dbo].[EquipeJoueur] (FK_Id_Equipe, Fk_Id_Utilisateur)
 values 
-('', ''),
-('', ''),
+-- c96bbb6a-36a5-4584-a38e-7cae7e19d9b7 --'6B29FC40-CA47-1067-B31D-00DD010662DA', 'Carufel', 'Kevin'
+('c96bbb6a-36a5-4584-a38e-7cae7e19d9b7', '6B29FC40-CA47-1067-B31D-00DD010662DA'),
+-- c96bbb6a-36a5-4584-a38e-7cae7e19d9b7 --'6B29FC40-CA47-1067-B31D-00DD010662DA', 'Carufel', 'Kevin'
+('c96bbb6a-36a5-4584-a38e-7cae7e19d9b7', '6B29FC40-CA47-1067-B31D-00DD010662DA'),
+
 ('', ''),
 ('', ''),
 
