7c4f7e5b0777b2806fd40b2fd9441578f68cbd45
Oleksii <36396435+Olmehan@users.noreply.github.com>
2023-05-17 17:09:21 -0400

    ajout controller

---
+++ GestionEquipeDeSports/GES_API/Controllers/AbonnerCalendrier.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GES_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AbonnerCalendrier : ControllerBase
+    {
+        // Get: api/<AbonnerCalendrierController/5
+        [HttpGet("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public ActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
