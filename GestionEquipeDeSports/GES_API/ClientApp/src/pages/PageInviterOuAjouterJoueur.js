09c0442798bcb15ca26c7c61d8ee3053c03a8ee9
Habil-Ndiamoun <85562269+Habil-Ndiamoun@users.noreply.github.com>
2023-05-18 11:49:53 -0400

    update

--- GestionEquipeDeSports/GES_API/ClientApp/src/pages/PageInviterOuAjouterJoueur.js
+++ GestionEquipeDeSports/GES_API/ClientApp/src/pages/PageInviterOuAjouterJoueur.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 import { useParams, useNavigate, Link } from 'react-router-dom';
+import iconeInvitation from '../images/iconeInvitation.png'; 
+import codeEnGuid from '../images/codeEnGuid.png'
 
 function PageInviterOuAjouterJoueur(){
     const navigate = useNavigate();
     const { id } = useParams();
 
-
   return (
     <Container>
-
         <Row>
             <h2 style={{textAlign: "center"}}>Inviter ou Ajouter de nouveaux membres</h2>
         </Row>
@@ -17,48 +17,38 @@ function PageInviterOuAjouterJoueur(){
         <p></p>
 
         <Row>
-
             <Col>
-                
-
-                    <Card style={{ width: '20rem' }}>
-
-                        <Card.Img variant="top" src="holder.js/100px180" />
-
-                        <Card.Body>
-
-                            <Button variant="dark" onClick={() => navigate(`/formulaireUtilisateur/${id}`)}>
-                                <Card.Title>Inviter de nouveaux membres</Card.Title>
-                            </Button>
-                            
-
-                            <Card.Text>
-                                Récupérez l'id de l"équipe pour l'envoyer par email aux membres de l'équipe.
-                            </Card.Text>
-
-                        </Card.Body>
-
-                    </Card>
-
-
-                
+                <Card style={{ width: '20rem' }}>
+                    <Card.Img variant="top" src={codeEnGuid} height={150} width={100}/>
+                    <Card.Body>
+                        <Button variant="dark" onClick={() => navigate(`/formulaireUtilisateur/${id}`)}>
+                            <Card.Title>Inviter de nouveaux membres</Card.Title>
+                        </Button>
+                        <p></p>
+                        <p></p>
+                        <Card.Text>
+                            Récupérez l'id de l"équipe pour l'envoyer par email aux membres de l'équipe.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
             </Col>
 
             <Col>
-                <Link to={`/formulaireUtilisateur/${id}`}>
-                    Ajouter
-                </Link>
+                <Card style={{ width: '20rem' }}>
+                    <Card.Img variant="top" src={iconeInvitation} height={150} width={100} />
+                    <Card.Body>
+                        <Button variant="dark" onClick={() => navigate(`/formulaireUtilisateur/${id}`)}>
+                            <Card.Title>Ajouter de nouveaux membres</Card.Title>
+                        </Button>
+                        <p></p>
+                        <p></p>
+                        <Card.Text>
+                            Vous pouvez ajouter vos membres en saisissant leurs informations personnelles.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
             </Col>
-
-            
-        
-           
-
-
-            
-
         </Row>
-
     </Container>
   )
 }
