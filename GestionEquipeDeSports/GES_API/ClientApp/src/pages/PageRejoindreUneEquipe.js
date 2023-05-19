d49c16e3130ff0442e73b9407bae85894bec746c
Habil-Ndiamoun <85562269+Habil-Ndiamoun@users.noreply.github.com>
2023-05-19 10:09:47 -0400

    update

--- GestionEquipeDeSports/GES_API/ClientApp/src/pages/PageRejoindreUneEquipe.js
+++ GestionEquipeDeSports/GES_API/ClientApp/src/pages/PageRejoindreUneEquipe.js
@@ -1,33 +1,26 @@
 import React from "react";
-import { Container, Row, Col, Card, Button } from 'react-bootstrap';
+import { Container, Row } from 'react-bootstrap';
 
 function PageRejoindreUneEquipe(){
     return(
-        <Container>
+        <Container style={{ justifyContent: 'center'}}>
             <Row>
-                <h2 style={{textAlign: "center"}}>Comment rejoindre une équipe sur 
+                <h2 >Comment rejoindre une équipe sur 
                     <span className='text-success'> Gestion</span><span className='text-primary'>Equipe</span><span className='text-warning'>Sportive</span>
                 </h2>
             </Row>
             <p></p>
             <p></p>
-            <Row>
-                <Col>
-                    <h3 style={{textAlign: "center"}}>1. Le responsable d'équipe vous a invité par email.</h3><p></p>
-                    <p >Regardez dans vos emails si vous avez reçu une invitation.<br></br>Dans l'email, copiez l'id de l'équipe et coller le dans le champ ci-dessous.</p>
+            <Row >
+                <h3>1. Le responsable d'équipe vous a invité par email.</h3><p></p>
+                <p>Regardez dans vos emails si vous avez reçu une invitation.<br></br>Dans l'email, copiez l'id de l'équipe et collez le dans le champ ci-dessous, puis cliquez sur Rejoindre.</p>
 
-                    <div>
-                        <input type="guid" className="form-control" name="identifiant" placeholder="Rentrer l'id de l'équipe" required />
-                        <p></p>
-                        <button type="button" className="btn btn-primary">Rejoindre</button>
-                    </div>
-                </Col>
+                <div className="col-xl-10">
+                    <input type="guid" className="form-control" name="identifiant" placeholder="Rentrer l'id de l'équipe" required />
+                    <p></p>
+                    <button type="button" className="btn btn-primary">Rejoindre</button>
+                </div>
             </Row>
-         
-            
-
-
-
         </Container>
     );
 }
