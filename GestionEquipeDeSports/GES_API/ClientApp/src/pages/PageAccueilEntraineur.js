ac3e6cb1ab2cb87eecbbf8ced26e7f8780f1b5d3
Habil-Ndiamoun <85562269+Habil-Ndiamoun@users.noreply.github.com>
2023-05-24 14:47:02 -0400

    update

--- GestionEquipeDeSports/GES_API/ClientApp/src/pages/PageAccueilEntraineur.js
+++ GestionEquipeDeSports/GES_API/ClientApp/src/pages/PageAccueilEntraineur.js
@@ -142,10 +142,12 @@ export const PageAcceuilEntraineur = () => {
                 </Col>
                 <Col>
                     {exporterVersICal()}
-                    <p>Pour vous abonner: 
-                        <Button variant="info" onClick={() => {copierLeLien()}} title="AbonnerCalendrier/VotreId" >Copier le lien</Button>
-                        {check && <FcApproval />}
-                    </p>
+                    <div className="float-end">
+                        <p>Pour vous abonner: 
+                            <Button variant="info" onClick={() => {copierLeLien()}} title="AbonnerCalendrier/VotreId" >Copier le lien</Button>
+                            {check && <FcApproval />}
+                        </p>
+                    </div>
                 </Col>
             </Row>
             <Row style={{marginTop: "1.0em"}}>
