50c87ae13767fd0fd2b8f9ae6d118fe7c51ffa89
Oleksii <36396435+Olmehan@users.noreply.github.com>
2023-05-29 14:43:40 -0400

    update adresse

--- GestionEquipeDeSports/GES_API/ClientApp/src/pages/PageAccueilEntraineur.js
+++ GestionEquipeDeSports/GES_API/ClientApp/src/pages/PageAccueilEntraineur.js
@@ -100,7 +100,8 @@ export const PageAcceuilEntraineur = () => {
 
     function copierLeLien() {
         // il faudra changer ce lien pour publier vers Azur, car il est évident que l'adresse sera différente
-        navigator.clipboard.writeText(`https://localhost:44474/api/AbonnerCalendrier/${idUtilisateur}`);
+        // https://gestionequipesportive.azurewebsites.net/api/AbonnerCalendrier/${idUtilisateur}
+        navigator.clipboard.writeText(`https://localhost:7225/api/AbonnerCalendrier/${idUtilisateur}`);
         setCheck(true);
         setSeconds(2);
     }
